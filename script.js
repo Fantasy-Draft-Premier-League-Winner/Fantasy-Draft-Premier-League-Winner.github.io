50ddfc553173832a2c0f2daf4a5a561d333c90be
Kaden Shin <kadenshin@Kadens-MacBook-Pro-3.local>
2024-03-09 21:58:31 -0800

    asdf

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('userIDform').addEventListener('submit', function (event) {
   event.preventDefault();
   var userID = document.getElementById('userID').value;
-  console.log('User ID:', userId);
+  console.log('User ID:', userID);
 });
