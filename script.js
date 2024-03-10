154e1c7379ad999c8d7c0a25360458dcad282686
Kaden Shin <kadenshin@Kadens-MacBook-Pro-3.local>
2024-03-09 22:37:56 -0800

    asd

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-document.getElementById('userIDform').addEventListener('submit', function (event) {
+/*document.getElementById('userIDform').addEventListener('submit', function (event) {
   event.preventDefault();
   var userID = document.getElementById('userID').value;
   console.log('User ID:', userID);
-});
+});*/
+function getTeamInfo() {
+  const userID = document.getElementById('userID').value;
+  const userTeamURL = `https://draft.premierleague.com/api/draft/entry/${userID}/transactions`;
+  if (!userID) {
+      alert('Please enter your ID');
+      return;
+  }
+  fetch(userTeamURL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+    // Work with the JSON data here
+      displayTeam(data);
+    })
+    .catch(error => {
+      console.error('There was a problem fetching the data:', error);
+    });
+}
+function displayTeam(data) { 
+  const myLineup = document.getElementById('myLineup');
+
+  data.forEach(picks => {
+    const player = document.createElement('div');
+    player.textContent = `Position: ${picks.position}`; // Example properties from the JSON object
+    myLineup.appendChild(player);
+  });
+  
+}
